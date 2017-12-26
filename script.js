f35017b23a1c765344c1850a5713d67a83244589
Tyler Fuqua <tyler@agilityda.com>
2017-12-25 20:32:22 -0700

    Hands On Project 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hands On Project 1
+// Functions
+
+var fullString = addStrings ("I am ", "a full string!");
+
+function addStrings (string1, string2 ) {
+    return string1 + string2;
+}
+
+console.log(fullString);
