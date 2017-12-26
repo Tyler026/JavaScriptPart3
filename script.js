0c04f0436334e54d050206b75ba1484b126c777c
Tyler Fuqua <tyler@agilityda.com>
2017-12-25 21:03:12 -0700

    Hands On Project 2

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // Hands On Project 1
 // Functions
 
-var fullString = addStrings ("I am ", "a full string!");
+/* var fullString = addStrings ("I am ", "a full string!");
 
 function addStrings (string1, string2 ) {
     return string1 + string2;
 }
 
-console.log(fullString);
+console.log(fullString); */
+
+/* Remember that variables that live within a function cannot be executed till the function is invoked. 
+You have to call the function outside the function like at the bottom. */
+
+function numberCalculation (number1, number2) {
+    var number1 = 45; 
+    var number2 = 78;
+    var multiplyNumbers = number1 * number2;
+
+    if (multiplyNumbers < 2000) {
+        alert( "I wish it was a bigger number");
+    } else {
+        alert( "That's more like it!");
+    }
+}
+
+numberCalculation();
